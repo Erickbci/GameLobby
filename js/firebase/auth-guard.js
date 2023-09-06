85982bd3c4ffe1ecb74b6ddd9d07f16f428173d9
Erickbci <batista.erick07@hotmail.com>
2023-09-06 18:47:08 -0300

    Editando CSS Jogo da velha

--- js/firebase/auth-guard.js
+++ js/firebase/auth-guard.js
@@ -1,7 +1,15 @@
 firebase.auth().onAuthStateChanged(user => {
-    if (!user) {
-      window.location.href = '../../index.html';
-    }
+    if (!user && window.location !== 'http://127.0.0.1:5500/index.html') {
+      
+      window.location.href === '../../index.html'
+       unconnectedUserSettings()
+     }  else {
+      unconnectedUserSettings()
+     }
   })
 
+  function unconnectedUserSettings() {
+    const unconnectedUserSettingsDiv = document.querySelector('.header-buttons');
+    unconnectedUserSettingsDiv.style.display = 'flex'
+  }
   
